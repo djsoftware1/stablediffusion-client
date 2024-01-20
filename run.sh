7b05b77f635df1dc3c933639794b5e19f37331ae
davidjoffe <david@djoffe.com>
2024-01-20 16:07:26 +0200

    Batch run helper updates

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# David Joffe
+
+counter=0
+
 while IFS= read -r line
 do
-  echo CALL python3 djgenerate.py \"$line\"
+  ((counter++))
+  echo CALL python3 $counter djgenerate.py \"$line\"
+  date>>_log.txt
+  echo CALL generate $counter \"$line\" >>_log.txt
   python3 djgenerate.py "$line"
 done < input.txt
